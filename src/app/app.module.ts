29c74678766b31a2ac29e85a48f92ad7b1c44824
Onbashi-Meriem <67912905+Onbashi-Meriem@users.noreply.github.com>
2022-04-27 07:42:39 +0200

    Add files via upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PlaceholderModule } from './placeholder/placeholder.module'
+import {TablesModule} from './tables/tables.module';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component'
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    NotFoundComponent
+  ],
+  imports: [
+    BrowserModule,
+    // PlaceholderModule,
+    TablesModule,
+    AppRoutingModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
